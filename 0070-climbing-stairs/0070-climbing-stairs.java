f9ed633f3811a5ec42304709297051ad0e2a9b4b
Abhi81204 <123285670+Abhi81204@users.noreply.github.com>
2024-04-05 21:23:04 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (87.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int a=1, b=2, sum=0;
+        for(int i=2; i<n; i++){
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+}
