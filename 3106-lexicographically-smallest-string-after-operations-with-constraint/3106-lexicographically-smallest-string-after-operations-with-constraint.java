3fbb7c47d268f1cce7f0407a394adeac86e2698f
Abhi81204 <123285670+Abhi81204@users.noreply.github.com>
2024-04-08 10:34:13 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (75.00%) - LeetHub

---
+++ 3106-lexicographically-smallest-string-after-operations-with-constraint/3106-lexicographically-smallest-string-after-operations-with-constraint.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String getSmallestString(String s, int k) {
+        char[] t = s.toCharArray();
+        for (int i = 0; i < t.length; i++) {
+            int currentDist = distToA(t[i]);
+            if (k >= currentDist) {
+                k -= currentDist;
+                t[i] = 'a';
+            } else {
+                if (k > 0) {
+                    if (t[i] - k >= 'a') t[i] = (char) (t[i] - k);
+                    else t[i] = (char) ('z' - (k - 1));
+                    k = 0;
+                }
+            }
+        }
+        return new String(t);
+    }
+    private int distToA(char c) {
+        return Math.min(c - 'a', 26 - (c - 'a'));
+    }
+}
