26c72c62dd6d3038e5dc07c22e3d4439a93a6a60
Abhi81204 <123285670+Abhi81204@users.noreply.github.com>
2024-04-09 08:20:19 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (25.83%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -2,13 +2,9 @@ class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int count = 0;
         for(int i=0; i<tickets.length; i++){
-            if(tickets[i]<tickets[k]) {
-                count += tickets[i];
-            } else if(i<=k) {
-                count += tickets[k];
-            } else {
-                count += tickets[k]-1;
-            }
+            if(tickets[i]<tickets[k]) count += tickets[i];
+            else if(i<=k) count += tickets[k];
+            else count += tickets[k]-1;
         }
         return count;
     }
