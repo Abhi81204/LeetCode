95468a18dc94ede38d79772307d0260add19f4d6
Abhi81204 <123285670+Abhi81204@users.noreply.github.com>
2024-07-05 10:29:14 +0530

    Time: 3 ms (84.75%), Space: 49.2 MB (63.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        if(grid==null || grid.length == 0) return 0;
+        int numOfIslands = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1'){
+                    numOfIslands++;
+                    backtrack(grid, i, j);
                 }
             }
         }
-        
-        return numIslands;
+        return numOfIslands;
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
-        }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+    public void backtrack(char grid[][], int i, int j){
+        if((i<0 || i>=grid.length) || (j<0 || j>=grid[0].length) || grid[i][j]!='1') return;
+        grid[i][j]='0';
+        backtrack(grid, i, j+1);
+        backtrack(grid, i, j-1);
+        backtrack(grid, i+1, j);
+        backtrack(grid, i-1, j);
     }
 }
+    
