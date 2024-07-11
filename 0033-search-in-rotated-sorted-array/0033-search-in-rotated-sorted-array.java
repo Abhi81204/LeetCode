fb8c143f8e76cac5d3877cfcfd1ccc03164592c3
Abhi81204 <123285670+Abhi81204@users.noreply.github.com>
2024-07-11 14:06:59 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (13.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,19 @@
 class Solution {
-    public static int binarySearch(int[] nums, int target, int l, int h){
-        while(l<=h){
-            int mid=(l+h)/2;
+    public int search(int[] nums, int target) {
+        int r = nums.length-1;
+        int l = 0;
+        while(l<=r){
+            int mid = l + (r - l) / 2;
             if(nums[mid]==target) return mid;
-            if(nums[mid]>target) h=mid-1;
-            else l=mid+1;
+            if(nums[mid]>=nums[l]) {
+                if(target<nums[mid] && target>=nums[l]) r = mid-1;
+                else l = mid+1;
+            } else {
+                if(target>nums[mid] && target<=nums[r]) l = mid+1;
+                else r = mid-1;
+            }
         }
         return -1;
     }
-    public int search(int[] nums, int target) {
-        int l =0;
-        int h=nums.length-1;
-        int m=-1;
-        while(l<=h){
-            int mid = (l+h)/2;
-            if(mid<h && nums[mid] > nums[mid+1]) m=mid;
-            if(mid>l && nums[mid] < nums[mid-1]) m=mid-1;
-            if(nums[l]>=nums[mid]) h=mid-1;
-            if(nums[l]<nums[mid]) l=mid+1;
-        }
-        if(m==-1) return binarySearch(nums, target, 0, nums.length-1);
-        if(nums[m]==target) return m;
-        if(nums[0]>target) return binarySearch(nums, target, m+1, nums.length-1);
-        else return binarySearch(nums, target, 0, m-1);
-
-    }
 }
+
